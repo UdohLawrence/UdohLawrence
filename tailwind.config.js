6b5b2e1e3161c702930bc1b8a018630431d5bbc3
Udoh Lawrence <lawrenceaudoh@gmail.com>
2024-07-06 20:33:24 +0100

    css code for mobile menu animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ module.exports = {
   content: ['./build/*.html'],
   theme: {
     extend: {
-      backgroundImage: {
-        'home-bg': "url('/build/images/home-bg.jpg')",
-        
+      screens: {
+        'widescreen': {'raw': '(min-aspect-ratio: 3/2'},
+        'tallscreen': {'raw': '(min-aspect-ratio: 13/20'},
+      },
+      keyframes: {
+        'open-menu': {
+          '0%': { transform:'scaleY(0)' },
+          '70%': { transform:'scaleY(1.3)' },
+          '100%': { transform:'scaleY(1)' },
+        },
+      },
+      animation: {
+        'open-menu': 'open-menu 0.5s ease-in-out forwards',
       }
     },
   },
   plugins: [],
 }
-
